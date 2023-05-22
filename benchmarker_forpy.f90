eb2c85b136cd37d86aee1e0d7d467e1261bba2ad
Simon Clifford <sjc306@cam.ac.uk>
2023-05-22 12:57:31 +0100

    Rename benchmarker to benchmarker_forpy

--- benchmarker_forpy.f90
+++ benchmarker_forpy.f90
@@ -8,4 +8,14 @@ if (ie .ne. 0) then
     write(*,*)'Forpy did not initialize, stopping'
     stop
 endif
+
+! Read in saved input (and output) values
+
+! Initialise the PyTorch model
+
+! Start timing
+
+! Run inference X many times (check output)
+
+! Stop timing, output results
 end program benchmarker
