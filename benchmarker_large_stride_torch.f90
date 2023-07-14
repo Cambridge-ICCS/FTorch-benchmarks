94e77832a63d170a5b645f7ed43aa01c6f47f984
Sophie Turner <st838@cam.ac.uk>
2023-07-14 16:31:20 +0100

    Added timings.

--- benchmarker_large_stride_torch.f90
+++ benchmarker_large_stride_torch.f90
@@ -5,12 +5,13 @@ use ftorch
 
 implicit none
 
-! Make a large array of 100,000,000 elements.
-integer, parameter :: ni = 10000, nj = 10000 
+! Make a large array of 1,000,000 elements.
+integer, parameter :: ni = 1000, nj = 1000 
 integer :: i
 integer(c_int), parameter :: n_inputs = 1
 integer(c_int64_t) :: shape_2d(2)
 integer(c_int) :: stride_2d(2)
+real :: start_time, end_time
 real, dimension(:,:), allocatable, target :: big_array, big_result
 
 type(torch_tensor) :: result_tensor
@@ -30,7 +31,11 @@ stride_2d = (/ 1, 2 /)
 input_array(1) = torch_tensor_from_blob(c_loc(big_array), 2, shape_2d, torch_kFloat32, torch_kCPU, stride_2d)
 result_tensor = torch_tensor_from_blob(c_loc(big_result), 2, shape_2d, torch_kFloat32, torch_kCPU, stride_2d)
 
+call cpu_time(start_time)
+
 call torch_module_forward(model, input_array, n_inputs, result_tensor)
+
+call cpu_time(end_time)
 
 ! Clean up.
 call torch_tensor_delete(result_tensor)
@@ -38,6 +43,8 @@ do i = 1, n_inputs
   call torch_tensor_delete(input_array(i))
 end do
 call torch_module_delete(model)
+
+write(*,*)"Time taken: ", end_time-start_time
 
 end program
 
