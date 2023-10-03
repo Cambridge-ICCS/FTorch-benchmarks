a2dfd6fe5e4d80cea687c4264fb4a43ab6898a8e
ElliottKasoar <ElliottKasoar@users.noreply.github.com>
2023-10-03 16:38:50 +0100

    Use realistic example with FTorch benchmark

--- benchmarker_resnet_torch.f90
+++ benchmarker_resnet_torch.f90
@@ -1,79 +1,166 @@
 program benchmark_resnet_test
 
-  use, intrinsic :: iso_c_binding
+  ! Imports primitives used to interface with C
+  use, intrinsic :: iso_c_binding, only: c_sp=>c_float, c_dp=>c_double, c_int64_t, c_null_char, c_loc
   use :: omp_lib, only : omp_get_wtime
-  use :: utils, only : assert_real_2d, setup, print_time_stats
+  use :: utils, only : assert_real, setup, print_time_stats
+  ! Import our library for interfacing with PyTorch
   use :: ftorch
+  use, intrinsic :: iso_fortran_env, only : sp => real32, dp => real64
 
   implicit none
 
-  integer :: i, ii, n
-  double precision :: start_time, end_time
-  double precision, allocatable :: durations(:)
+  ! Define working precision for C primitives
+  ! Precision must match `wp` in resnet18.py and `wp_torch` in pt2ts.py
+  integer, parameter :: c_wp = c_sp
+  integer, parameter :: wp = sp
+  integer, parameter :: torch_wp = torch_kFloat32
 
-  real(c_float), dimension(:,:,:,:), allocatable, target :: in_data
-  integer(c_int), parameter :: n_inputs = 1
-  real(c_float), dimension(:,:), allocatable, target :: out_data
+  call main()
 
-  integer(c_int), parameter :: in_dims = 4
-  integer(c_int64_t) :: in_shape(in_dims) = [1, 3, 224, 224]
-  integer(c_int) :: in_layout(in_dims) = [1,2,3,4]
-  integer(c_int), parameter :: out_dims = 2
-  integer(c_int64_t) :: out_shape(out_dims) = [1, 1000]
-  integer(c_int) :: out_layout(out_dims) = [1,2]
+  contains
 
-  character(len=:), allocatable :: model_dir, model_name
-  character(len=128) :: msg
-  integer :: ntimes
+    subroutine main()
 
-  type(torch_module) :: model
-  type(torch_tensor), dimension(1) :: in_tensor
-  type(torch_tensor) :: out_tensor
+    integer :: i, ii, n
+    double precision :: start_time, end_time
+    double precision, allocatable :: durations(:)
 
-  print *, "====== DIRECT COUPLED ======"
+    real(c_wp), dimension(:,:,:,:), allocatable, target :: in_data
+    integer(c_int), parameter :: n_inputs = 1
+    real(c_wp), dimension(:,:), allocatable, target :: out_data
 
-  call setup(model_dir, model_name, ntimes, n)
+    integer(c_int), parameter :: in_dims = 4
+    integer(c_int64_t) :: in_shape(in_dims) = [1, 3, 224, 224]
+    integer(c_int) :: in_layout(in_dims) = [1,2,3,4]
+    integer(c_int), parameter :: out_dims = 2
+    integer(c_int64_t) :: out_shape(out_dims) = [1, 1000]
+    integer(c_int) :: out_layout(out_dims) = [1,2]
 
-  allocate(in_data(in_shape(1), in_shape(2), in_shape(3), in_shape(4)))
-  allocate(out_data(out_shape(1), out_shape(2)))
-  allocate(durations(ntimes))
+    character(len=:), allocatable :: model_dir, model_name
+    character(len=128) :: msg
+    integer :: ntimes
 
-  model = torch_module_load(model_dir//"/"//model_name//C_NULL_CHAR)
+    type(torch_module) :: model
+    type(torch_tensor), dimension(1) :: in_tensor
+    type(torch_tensor) :: out_tensor
 
-  do i = 1, ntimes
+    ! Binary file containing input tensor
+    character(len=*), parameter :: filename = '/home/ek/ICCS/fortran-pytorch-lib/examples/2_ResNet18/data/image_tensor.dat'
 
-    ! Initialise data
-    in_data = 1.0d0
+    ! Length of tensor and number of categories
+    integer, parameter :: tensor_length = 150528
 
-    start_time = omp_get_wtime()
+    ! Outputs
+    integer :: index(2)
+    real(wp), dimension(:,:), allocatable :: probabilities
+    real(wp), parameter :: expected_prob = 0.8846225142478943
+    real(wp) :: probability
 
-    ! Create input and output tensors for the model.
-    in_tensor(1) = torch_tensor_from_blob(c_loc(in_data), in_dims, in_shape, torch_kFloat32, torch_kCPU, in_layout)
-    out_tensor = torch_tensor_from_blob(c_loc(out_data), out_dims, out_shape, torch_kFloat32, torch_kCPU, out_layout)
+    print *, "====== DIRECT COUPLED ======"
 
-    call torch_module_forward(model, in_tensor, n_inputs, out_tensor)
+    call setup(model_dir, model_name, ntimes, n)
 
-    ! Clean up.
-    call torch_tensor_delete(out_tensor)
-    do ii = 1, n_inputs
-      call torch_tensor_delete(in_tensor(ii))
+    allocate(in_data(in_shape(1), in_shape(2), in_shape(3), in_shape(4)))
+    allocate(out_data(out_shape(1), out_shape(2)))
+    allocate(durations(ntimes))
+    allocate(probabilities(out_shape(1), out_shape(2)))
+
+    model = torch_module_load(model_dir//"/"//model_name//C_NULL_CHAR)
+
+    ! Initialise data - previously in loop, but not modified?
+    call load_data(filename, tensor_length, in_data)
+
+    do i = 1, ntimes
+
+      start_time = omp_get_wtime()
+
+      ! Create input and output tensors for the model.
+      in_tensor(1) = torch_tensor_from_blob(c_loc(in_data), in_dims, in_shape, torch_kFloat32, torch_kCPU, in_layout)
+      out_tensor = torch_tensor_from_blob(c_loc(out_data), out_dims, out_shape, torch_kFloat32, torch_kCPU, out_layout)
+
+      call torch_module_forward(model, in_tensor, n_inputs, out_tensor)
+
+      ! Clean up.
+      call torch_tensor_delete(out_tensor)
+      do ii = 1, n_inputs
+        call torch_tensor_delete(in_tensor(ii))
+      end do
+
+      end_time = omp_get_wtime()
+
+      ! Calculate probabilities and output results
+      call calc_probs(out_data, probabilities)
+      index = maxloc(probabilities)
+      probability = maxval(probabilities)
+
+      ! Check top probability matches expected value
+      call assert_real(probability, expected_prob, test_name="Check probability", rtol_opt=1e-5)
+
+      durations(i) = end_time-start_time
+      ! the forward model is deliberately non-symmetric to check for difference in Fortran and C--type arrays.
+      write(msg, '(A, I8, A, F10.3, A)') "check iteration ", i, " (", durations(i), " s) [omp]"
+      print *, trim(msg)
     end do
 
-    end_time = omp_get_wtime()
-
-    durations(i) = end_time-start_time
-    ! the forward model is deliberately non-symmetric to check for difference in Fortran and C--type arrays.
-    write(msg, '(A, I8, A, F10.3, A)') "check iteration ", i, " (", durations(i), " s) [omp]"
-    print *, trim(msg)
-  end do
-
-  call print_time_stats(durations)
+    call print_time_stats(durations)
 
 
-  call torch_module_delete(model)
+    call torch_module_delete(model)
 
-  deallocate(in_data)
-  deallocate(out_data)
-  deallocate(durations)
+    deallocate(in_data)
+    deallocate(out_data)
+    deallocate(durations)
+    deallocate(probabilities)
+
+  end subroutine main
+
+  subroutine load_data(filename, tensor_length, in_data)
+
+    implicit none
+
+    character(len=*), intent(in) :: filename
+    integer, intent(in) :: tensor_length
+    real(c_wp), dimension(:,:,:,:), intent(out) :: in_data
+
+    real(c_wp) :: flat_data(tensor_length)
+    integer :: ios
+    character(len=100) :: ioerrmsg
+
+    ! Read input tensor from Python script
+    open(unit=10, file=filename, status='old', access='stream', form='unformatted', action="read", iostat=ios, iomsg=ioerrmsg)
+    if (ios /= 0) then
+    print *, ioerrmsg
+    stop 1
+    end if
+
+    read(10, iostat=ios, iomsg=ioerrmsg) flat_data
+    if (ios /= 0) then
+        print *, ioerrmsg
+        stop 1
+    end if
+
+    close(10)
+
+    ! Reshape data to tensor input shape
+    ! This assumes the data from Python was transposed before saving
+    in_data = reshape(flat_data, shape(in_data))
+
+  end subroutine load_data
+
+  subroutine calc_probs(out_data, probabilities)
+
+    implicit none
+
+    real(c_wp), dimension(:,:), intent(in) :: out_data
+    real(wp), dimension(:,:), intent(out) :: probabilities
+    real(wp) :: prob_sum
+
+    ! Apply softmax function to calculate probabilties
+    probabilities = exp(out_data)
+    prob_sum = sum(probabilities)
+    probabilities = probabilities / prob_sum
+
+  end subroutine calc_probs
 
 end program benchmark_resnet_test
