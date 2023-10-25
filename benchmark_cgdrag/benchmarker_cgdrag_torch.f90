b3ad0abd47038a3ba22e3b3385d1dff700a271ec
ElliottKasoar <ElliottKasoar@users.noreply.github.com>
2023-10-25 16:19:24 +0100

    Reshape cgdrag torch output explicitly

--- benchmark_cgdrag/benchmarker_cgdrag_torch.f90
+++ benchmark_cgdrag/benchmarker_cgdrag_torch.f90
@@ -27,6 +27,7 @@ program benchmark_cgdrag_test
 
   real(wp), dimension(:,:), allocatable, target :: uuu_flattened, vvv_flattened
   real(wp), dimension(:,:), allocatable, target :: lat_reshaped, psfc_reshaped
+  real(wp), dimension(:,:), allocatable, target  :: gwfcng_x_flattened, gwfcng_y_flattened
 
   integer(c_int), parameter :: n_inputs = 3
 
@@ -70,6 +71,8 @@ program benchmark_cgdrag_test
   allocate( vvv_flattened(I_MAX*J_MAX, K_MAX) )
   allocate( lat_reshaped(I_MAX*J_MAX, 1) )
   allocate( psfc_reshaped(I_MAX*J_MAX, 1) )
+  allocate( gwfcng_x_flattened(I_MAX*J_MAX, K_MAX) )
+  allocate( gwfcng_y_flattened(I_MAX*J_MAX, K_MAX) )
 
   ! Read in saved input (and output) values
   open(10, file='../cgdrag_model/uuu.txt')
@@ -127,7 +130,7 @@ program benchmark_cgdrag_test
 
     ! Zonal
     in_tensors(1) = torch_tensor_from_blob(c_loc(uuu_flattened), dims_2D, shape_2D, torch_wp, torch_kCPU, stride_2D)
-    gwfcng_x_tensor = torch_tensor_from_blob(c_loc(gwfcng_x), dims_out, shape_out, torch_wp, torch_kCPU, stride_out)
+    gwfcng_x_tensor = torch_tensor_from_blob(c_loc(gwfcng_x_flattened), dims_out, shape_out, torch_wp, torch_kCPU, stride_out)
     end_time = omp_get_wtime()
     durations(i, 2) = end_time - start_time
     ! ------------------------------ End tensor timer ------------------------------
@@ -144,7 +147,7 @@ program benchmark_cgdrag_test
     ! ------------------------------ Start tensor timer ------------------------------
     start_time = omp_get_wtime()
     in_tensors(1) = torch_tensor_from_blob(c_loc(vvv_flattened), dims_2D, shape_2D, torch_wp, torch_kCPU, stride_2D)
-    gwfcng_y_tensor = torch_tensor_from_blob(c_loc(gwfcng_y), dims_out, shape_out, torch_wp, torch_kCPU, stride_out)
+    gwfcng_y_tensor = torch_tensor_from_blob(c_loc(gwfcng_y_flattened), dims_out, shape_out, torch_wp, torch_kCPU, stride_out)
     end_time = omp_get_wtime()
     durations(i, 2) = durations(i, 2) + (end_time - start_time)
     ! ------------------------------ End tensor timer ------------------------------
@@ -156,6 +159,12 @@ program benchmark_cgdrag_test
     end_time = omp_get_wtime()
     durations(i, 3) = durations(i, 3) + (end_time - start_time)
     ! ------------------------------ End inference timer ------------------------------
+
+    ! Reshape, and assign to gwfcng
+    do j=1,J_MAX
+      gwfcng_x(:,j,:) = gwfcng_x_flattened((j-1)*I_MAX+1:j*I_MAX,:)
+      gwfcng_y(:,j,:) = gwfcng_y_flattened((j-1)*I_MAX+1:j*I_MAX,:)
+    end do
 
     ! Clean up.
     ! ------------------------------ Start tensor timer ------------------------------
@@ -203,6 +212,8 @@ program benchmark_cgdrag_test
   deallocate(vvv_flattened)
   deallocate(lat_reshaped)
   deallocate(psfc_reshaped)
+  deallocate(gwfcng_x_flattened)
+  deallocate(gwfcng_y_flattened)
   deallocate(gwfcng_x_ref)
   deallocate(gwfcng_y_ref)
 
